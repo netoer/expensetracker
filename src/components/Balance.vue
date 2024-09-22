c3f3bf46b333ae70fb614e60f778288c70c70610
netoer <vidalernesto322@gmail.com>
2024-09-21 19:54:03 -0500

    first commit

---
+++ src/components/Balance.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Tu saldo es:</h1> 
+    <h2 id="balance">${{total}}</h2>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    total: {
+        type: Number,
+        Required: true
+    }
+})
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3>Historial</h3>
+        <ul id="list" class="list">
+            <li v-for="transaction in transactions" v-bind:key="transaction.id">
+                {{ transaction.text }} 
+                <span>-${{ transaction-amount }}</span>
+                <button class="deleta-btn">X</button>
+            </li>
+            <button class="delate-btn"></button>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true,
+    }
+
+
+});
+</script>
+
